7983909ebd3d3e01b0158dea4a52551b79a17cae
buptpal <925978482@qq.com>
2020-04-20 19:18:23 +0800

    tab-control

---
+++ components/m-tab-control/tab-control.js
@@ -0,0 +1,34 @@
+// components/m-tab-control/tab-control.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    titles: {
+      type: Array,
+      value: []
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    currentIndex: 0
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    habdleItemClick(event){
+      // 取出index
+      var index = event.currentTarget.dataset.index;
+      this.setData({
+        currentIndex: index
+      })
+      // 通知页面内部点击事件
+      this.triggerEvent('itemclick',{index,title:this.properties.titles[index]},{})
+    }
+  }
+})
