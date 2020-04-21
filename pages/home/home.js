62a8d29d713a98a375c14167badfc28ec3a0bf60
buptpal <925978482@qq.com>
2020-04-21 09:31:36 +0800

    获取商品数据

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,5 +1,5 @@
 // pages/home/home.js
-import { getMultidata } from '../../net/home.js'
+import { getMultidata,getGoodsdata } from '../../net/home.js'
 Page({
 
   /**
@@ -19,6 +19,17 @@ Page({
     console.log(event)
   },
   onLoad: function (options) {
+    //1. 请求轮播图
+    this._getMultidata()
+    // 2.请求商品数据
+    // this._getGoodsData('pop')
+    
+    //给流行精选设置假数据
+    this.setGoodsData('pop')
+  },
+
+  // ---------------网络请求函数-------------
+  _getMultidata(){
     getMultidata().then(res => {
       const banners = res.data.data.banner.list;
       const recommends = res.data.data.recommend.list;
@@ -28,6 +39,50 @@ Page({
       })
     })
   },
+//流行精选商品
+_getGoodsData(type){
+  //1根据type 获取页码
+  const page= this.data.goods[type].page+1;
+  //2发送网络请求
+  getGoodsdata(type,page).then(res =>{
+    //2.1取出返回的数据
+    const list=res.data.data.list;
+
+    //2.2将数据设置到对应的type的list中
+    const oldlist=this.data.goods[type].list;
+    //...list 是将list的每个元素给添加到list中，可以用循环来操作
+    // for(let item of list){
+    //   oldlist.push(item);
+    // }
+    oldlist.push(...list);
+
+    //2.3将数据设置到data的goods中
+    //获取good中的list
+    const typeKey = `goods.${type}.list`;
+    //获取goods中的page
+    const pageKey = `goods.${type}.page`;
+    this.setData({
+      [typeKey]:oldlist,
+      [pageKey]:page
+    })
+  })
+},
+
+//提供假数据
+setGoodsData(type){
+  const oldlist=this.data.goods[type].list;
+  for(var i= 0;i<30;i++){
+    const item = { title: '烟花烫2019秋季新款时尚淑女中式绣花立领衬衫', image:'https://img13.360buyimg.com/n1/s350x449_jfs/t1/51877/11/5741/206670/5d36c899Ea1cab487/87559e3f76d477c3.jpg!cc_350x449.jpg',price:199.9,cfav:208};
+    oldlist.push(item);
+    //设置数据
+    const listKey = `goods.${type}.list`;
+    this.setData({
+      [listKey]:oldlist
+    })
+  }
+},
+  // ---------------时间监听函数-------------
+
   handleTabclick(event){
     const index = event.detail.index;
     console.log(index)
